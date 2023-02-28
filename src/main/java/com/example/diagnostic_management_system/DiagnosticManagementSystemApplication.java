88911e30e3e69bbcc967ffd1cbebd9c4ae93e504
arafatrimon <rimonarafat2020@gmail.com>
2023-02-28 17:36:23 +0600

    first commit

---
+++ src/main/java/com/example/diagnostic_management_system/DiagnosticManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.diagnostic_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diagnostic_management_system/DiagnosticManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diagnostic_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
